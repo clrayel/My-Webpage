4112bb6a459dad2cb6ed8dc5b109c11b5403563e
Carlos Rayel <91717022+clrayel@users.noreply.github.com>
2024-08-21 16:06:46 +0800

    Basic profile created

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,22 @@
+import "./ProfileCard.css";
+import ProfileImage from "../assets/profile_image.jpg";
+
+function ProfileCard() {
+  return (
+    <div className="parent">
+      <div className="child">
+        <img src={ProfileImage} alt="profile image" className="profile image" />
+      </div>
+      <div className="child">
+        <h1 className="profile name">Carlos Angelo L. Rayel</h1>
+        <p className="profile details">
+          BS Computer Science graduate of the University of the Philippines, Los
+          Baños. Interested in Web Development and graphic design, but is open
+          to new job prospects.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
